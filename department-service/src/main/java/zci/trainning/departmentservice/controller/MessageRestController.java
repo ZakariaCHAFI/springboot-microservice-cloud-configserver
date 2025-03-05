d07c0efc728724f8e858ddd445ff4978c41798c3
Zakaria CHAFI <zchafi@gmail.com>
2025-03-05 02:03:49 +0000

    Spring Boot Microservices - Spring Cloud Config Server

--- department-service/src/main/java/zci/trainning/departmentservice/controller/MessageRestController.java
+++ department-service/src/main/java/zci/trainning/departmentservice/controller/MessageRestController.java
@@ -1,12 +1,13 @@
 package zci.trainning.departmentservice.controller;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+
 @RestController
+@RefreshScope
 public class MessageRestController {
     @Value("${key.message:Hello default}")
     private String message;
